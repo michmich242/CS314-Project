325210257ec1beef650e2403f007407795d3182f
Ray Cochrane <cochrane.ray@gmail.com>
2025-05-20 16:48:25 -0700

    Added class constructor which establishes connection with db

--- include/sqlengine.h
+++ include/sqlengine.h
@@ -1,52 +1,58 @@
 // SQL API for ChocAn Database
 //
 #pragma once
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <memory>
 #include <pqxx/pqxx>
+#include <sstream>
 #include <string>
 
 // Struct to pass back provider summaries
 // Passed back in a vector
 struct ProviderSummary {
-	std::string provider_id;
-	std::string provider_name;
-	int num_consultations;
-	float total_fee;
+  std::string provider_id;
+  std::string provider_name;
+  int num_consultations;
+  float total_fee;
 };
 
 struct ManagerSummary {
-	std::vector<ProviderSummary> providers;
-	int total_consultations;
-	float total_fees;
+  std::vector<ProviderSummary> providers;
+  int total_consultations;
+  float total_fees;
 };
 
 class SQLEngine {
-  public:
+public:
+  SQLEngine(const std::string &db_path = ".dbinfo");
+  bool is_connected() const;
 
-	// Member interactions
-	bool add_member(const Member &member);
-	bool update_member(const Member &member);
-	bool delete_member(const std::string &id);
-	bool validate_member(const std::string &id);
-	bool get_member_status(const std::string &id);
+  // Member interactions
+  bool add_member(const Member &member);
+  bool update_member(const Member &member);
+  bool delete_member(const std::string &id);
+  bool validate_member(const std::string &id);
+  bool get_member_status(const std::string &id);
 
-	// Provider Interactions
-	bool add_provider(const Provider &provider);
-	bool update_provider(const Provider &provider);
-	bool delete_provider(const std::string &id);
+  // Provider Interactions
+  bool add_provider(const Provider &provider);
+  bool update_provider(const Provider &provider);
+  bool delete_provider(const std::string &id);
+  bool validate_provider(const std::string &id);
 
-	// Service List and Records
-	bool save_service_record(const ServiceRecord &record);
-	Service get_service(const std::string &code);
-	std::vector<Service> get_all_services();
+  // Service List and Records
+  bool save_service_record(const ServiceRecord &record);
+  Service get_service(const std::string &code);
+  std::vector<Service> get_all_services();
 
-	// Weekly Reporting
-	std::vector<ServiceRecord> generate_member_service_reports();
-	std::vector<ServiceRecord> generate_provider_service_reports();
-	std::vector<ProviderSummary> generate_provider_summary_report();
-	ManagerSummary generate_manager_summary_reports();
+  // Weekly Reporting
+  std::vector<ServiceRecord> generate_member_service_reports();
+  std::vector<ServiceRecord> generate_provider_service_reports();
+  std::vector<ProviderSummary> generate_provider_summary_report();
+  ManagerSummary generate_manager_summary_reports();
 
-  private:
-
-	pqxx::connection conn;
+private:
+  std::unique_ptr<pqxx::connection> conn;
 };
--- src/sqlengine.cpp
+++ src/sqlengine.cpp
@@ -1,1 +1,80 @@
 #include "../include/sqlengine.h"
+
+// SQLEngine constructor, does not need an input unless overriding file path
+// default path: .dbinfo
+//
+// .dbinfo default style:
+// host=fake_host
+// port=5432
+// dbname=name
+// user=username
+// password=12345
+//
+SQLEngine::SQLEngine(const std::string &db_path) {
+  std::map<std::string, std::string> db_info;
+
+  // Open .dbinfo
+  std::ifstream infile(db_path);
+  if (!infile) {
+    std::cerr << "Failed to retrieve database info.\n" << std::endl;
+    return;
+  }
+
+  // Intake .dbinfo, use map to make key-value pairs
+  std::string key_value;
+  while (std::getline(infile, key_value)) {
+    size_t eq = key_value.find('=');
+    if (eq == std::string::npos)
+      continue;
+    std::string key = key_value.substr(0, eq);
+    std::string value = key_value.substr(eq + 1);
+    db_info[key] = value;
+  }
+
+  // Create String from .dbinfo lines
+  std::ostringstream conn_str;
+  conn_str << "host=" << db_info["host"] << " port=" << db_info["port"]
+           << " dbname=" << db_info["dbname"] << " user=" << db_info["user"]
+           << " password=" << db_info["password"];
+
+  // Attempt to connect with given key-value pairs
+  try {
+    // Assign Unique pointer
+    conn = std::make_unique<pqxx::connection>(conn_str.str());
+    if (!conn->is_open()) {
+      std::cerr << "Failed to open database connection.\n";
+    }
+  } catch (const std::exception &e) {
+    std::cerr << "Connection Failed: " << e.what() << "\n";
+  }
+}
+
+// Checks if our connection is working,
+// used as an err checker in main in case
+// constructor failed to connect
+bool SQLEngine::is_connected() const { return conn && conn->is_open(); }
+
+// Member interactions
+bool add_member(const Member &member) {}
+bool update_member(const Member &member) {}
+bool delete_member(const std::string &id) {}
+bool validate_member(const std::string &id) {}
+bool get_member_status(const std::string &id) {}
+
+// Provider Interactions
+bool add_provider(const Provider &provider) {}
+bool update_provider(const Provider &provider) {}
+
+bool delete_provider(const std::string &id) {}
+bool validate_provider(const std::string &id) {}
+
+// Service List and Records
+bool save_service_record(const ServiceRecord &record) {}
+Service get_service(const std::string &code) {}
+std::vector<Service> get_all_services() {}
+
+// Weekly Reporting
+std::vector<ServiceRecord> generate_member_service_reports() {}
+std::vector<ServiceRecord> generate_provider_service_reports() {}
+std::vector<ProviderSummary> generate_provider_summary_report() {}
+ManagerSummary generate_manager_summary_reports() {}
