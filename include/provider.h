78c1e187913c2dfc1e2251567c5fb3d803dfc4be
michmich242 <karammichel920@gmail.com>
2025-05-21 18:07:47 -0700

    Implemented the header file for the Provider.h

--- include/provider.h
+++ include/provider.h
@@ -1,3 +1,22 @@
+#pragma once
 #include "sqlengine.h"
 
 
+
+class Provider_User{
+    public:
+        Provider_User();
+        ~Provider_User();
+        
+        bool login();
+        void display_service_directory();
+        bool Post_Service_Billing();
+
+
+
+    private:
+        std::string provider_id;
+        std::string service_code;
+        std::string service_fee;
+
+};
